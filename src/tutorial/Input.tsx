a27055de51aede044c14c7f8f93ce315036eb068
Негодов Никита <nn@smarthead.ru>
2021-02-08 13:23:38 +0300

    Example with custom handler has added

--- src/tutorial/Input.tsx
+++ src/tutorial/Input.tsx
@@ -5,9 +5,15 @@ interface Props {
     label: string;
     fieldConfig: FieldHookConfig<string>;
     className?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Field: React.FC<Props> = ({ label, fieldConfig, className }) => {
+const Field: React.FC<Props> = ({
+    label,
+    fieldConfig,
+    className,
+    onChange,
+}) => {
     const [field, meta] = useField(fieldConfig);
     return (
         <div>
@@ -17,6 +23,7 @@ const Field: React.FC<Props> = ({ label, fieldConfig, className }) => {
                 {...field}
                 type={fieldConfig.type}
                 placeholder={fieldConfig.placeholder}
+                onChange={onChange ? onChange : field.onChange}
             />
             {meta.touched && meta.error ? (
                 <span className="error">{meta.error}</span>
--- src/tutorial/SignupForm.tsx
+++ src/tutorial/SignupForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Form } from 'formik';
+import { Formik, Form, FormikProps } from 'formik';
 import * as Yup from 'yup';
 
 import Input from './Input';
@@ -7,7 +7,25 @@ import Checkbox from './Checkbox';
 import Radio from './Radio';
 import Select from './Select';
 
+interface IForm {
+    firstName: string;
+    lastName: string;
+    sex: string;
+    music: never[];
+    jobType: string;
+    email: string;
+    accept: boolean;
+}
+
 const SignupForm: React.FC = () => {
+    const handleNameChange = (
+        e: React.ChangeEvent<HTMLInputElement>,
+        formik: FormikProps<IForm>
+    ) => {
+        formik.setFieldValue('firstName', e.target.value);
+        console.log(e.target.value);
+    };
+
     return (
         <Formik
             initialValues={{
@@ -43,63 +61,76 @@ const SignupForm: React.FC = () => {
                 alert(JSON.stringify(values, null, 2));
             }}
         >
-            <Form>
-                <Input
-                    label="First Name"
-                    fieldConfig={{
-                        name: 'firstName',
-                        type: 'text',
-                    }}
-                />
+            {(formik) => (
+                <Form>
+                    <Input
+                        label="First Name"
+                        fieldConfig={{
+                            name: 'firstName',
+                            type: 'text',
+                        }}
+                        onChange={(e) => handleNameChange(e, formik)}
+                    />
 
-                <Input
-                    label="Last Name"
-                    fieldConfig={{
-                        name: 'lastName',
-                        type: 'text',
-                    }}
-                />
-                <p>What is your sex?</p>
-                <Radio fieldConfig={{ name: 'sex', value: 'male' }}>Male</Radio>
-                <Radio fieldConfig={{ name: 'sex', value: 'female' }}>
-                    Female
-                </Radio>
+                    <Input
+                        label="Last Name"
+                        fieldConfig={{
+                            name: 'lastName',
+                            type: 'text',
+                        }}
+                    />
+                    <p>What is your sex?</p>
+                    <Radio fieldConfig={{ name: 'sex', value: 'male' }}>
+                        Male
+                    </Radio>
+                    <Radio fieldConfig={{ name: 'sex', value: 'female' }}>
+                        Female
+                    </Radio>
 
-                <p>What type of music do you listen?</p>
-                <Checkbox fieldConfig={{ name: 'music', value: 'Rock' }}>
-                    Rock
-                </Checkbox>
-                <Checkbox fieldConfig={{ name: 'music', value: 'Classic' }}>
-                    Classic
-                </Checkbox>
-                <Checkbox fieldConfig={{ name: 'music', value: 'Rep' }}>
-                    Jazz
-                </Checkbox>
+                    <p>What type of music do you listen?</p>
+                    <Checkbox fieldConfig={{ name: 'music', value: 'Rock' }}>
+                        Rock
+                    </Checkbox>
+                    <Checkbox fieldConfig={{ name: 'music', value: 'Classic' }}>
+                        Classic
+                    </Checkbox>
+                    <Checkbox fieldConfig={{ name: 'music', value: 'Jazz' }}>
+                        Jazz
+                    </Checkbox>
 
-                <Select label="Job Type" fieldConfig={{ name: 'jobType' }}>
-                    <option value="" disabled>
-                        Select a job type
-                    </option>
-                    <option value="designer">Designer</option>
-                    <option value="development">Developer</option>
-                    <option value="product">Product Manager</option>
-                    <option value="other">Other</option>
-                </Select>
+                    <Select label="Job Type" fieldConfig={{ name: 'jobType' }}>
+                        <option value="" disabled>
+                            Select a job type
+                        </option>
+                        <option value="designer">Designer</option>
+                        <option value="development">Developer</option>
+                        <option value="product">Product Manager</option>
+                        <option value="other">Other</option>
+                    </Select>
 
-                <Input
-                    label="Email"
-                    fieldConfig={{
-                        name: 'email',
-                        type: 'email',
-                    }}
-                />
+                    <Input
+                        label="Email"
+                        fieldConfig={{
+                            name: 'email',
+                            type: 'email',
+                        }}
+                    />
 
-                <Checkbox fieldConfig={{ name: 'accept' }}>
-                    I accept the terms and conditions
-                </Checkbox>
+                    <Checkbox fieldConfig={{ name: 'accept' }}>
+                        I accept the terms and conditions
+                    </Checkbox>
 
-                <button type="submit">Submit</button>
-            </Form>
+                    <button type="submit" onClick={formik.handleReset}>
+                        Reset
+                    </button>
+                    <button
+                        disabled={!(formik.isValid && formik.dirty)}
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </Form>
+            )}
         </Formik>
     );
 };
